fc1508529f8e7ee39fde124a42096590194af362
code-turpal <gireevanzor@gmail.com>
2023-09-02 21:44:06 +0300

    first commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,22 @@
 const menuSub = document.querySelectorAll('.menu_sub');
 const menuLink = document.querySelectorAll('.menu__link');
+let activeMenuSub;
 
-menuLink.forEach(link => {
-    link.onclick = () => {
+menuLink.forEach((link, index) => {
+    link.onclick = () => {  
+
         if (link.parentElement.querySelector('.menu_sub') !== null) {
-            if (link.closest('ul').querySelector('.menu_active') !== null) {
-                link.closest('ul').querySelector('.menu_active').classList.toggle('menu_active');
+            if (activeMenuSub === index) {
+                link.parentElement.querySelector('.menu_sub').classList.toggle('menu_active');
+                return false;
             }
-            link.parentElement.querySelector('.menu_sub').classList.toggle('menu_active');
+            if (link.closest('ul').querySelector('.menu_active') !== null) {
+                link.closest('ul').querySelector('.menu_active').classList.remove('menu_active');
+            } 
+
+            activeMenuSub = index;
+            
+            link.parentElement.querySelector('.menu_sub').classList.add('menu_active');
             return false;
         }
     }
