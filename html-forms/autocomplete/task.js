33670a50dc830a502dc236db552b74a8192a83e3
code-turpal <gireevanzor@gmail.com>
2023-09-09 16:06:11 +0300

    first commit autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,26 +68,19 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
-  }
-}
+    let array = [];
+    for (let i = 0; i < this.input.options.length; i++) {
+      if (this.input.options[i].text.includes(this.searchInput.value)) {
+        let dict =
+          {
+            text: this.input.options[i].text,
+            value: this.input.options[i].value,
+          };
+        array.push(dict);
+      };
+    };
+    return array;
+  };
+};
 
 new Autocomplete( document.querySelector( '.autocomplete' ));
