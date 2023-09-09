ca290d6e894dd2ada0db4513ce1492727121faf2
code-turpal <gireevanzor@gmail.com>
2023-09-09 17:26:10 +0300

    first commit tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,43 @@
+let activeTooltip = null;
+
+const elements = document.querySelectorAll(".has-tooltip");
+
+elements.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (activeTooltip && activeTooltip.innerText === item.title) {
+      activeTooltip.classList.toggle("tooltip_active");
+      return;
+    }
+
+    if (activeTooltip) {
+      activeTooltip.classList.remove("tooltip_active");
+      document.body.removeChild(activeTooltip);
+    }
+
+    const text = item.title;
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("tooltip");
+    tooltip.innerText = text;
+    document.body.appendChild(tooltip); 
+
+    const position = item.getBoundingClientRect();
+    tooltip.style.left = `${position.left}px`;
+    tooltip.style.top = `${position.bottom}px`;
+
+    activeTooltip = tooltip;
+    activeTooltip.classList.add("tooltip_active");
+  });
+});
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  if (!target.classList.contains("has-tooltip")) {
+    if (activeTooltip) {
+      activeTooltip.classList.remove("tooltip_active");
+      document.body.removeChild(activeTooltip);
+      activeTooltip = null;
+    }
+  }
+});
